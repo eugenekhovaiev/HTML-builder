182229f32e7e086e81db49b58ecae436d532600c
Yevhenii Khovaiev <ekhovaev@gmail.com>
2023-05-06 16:26:39 +0300

    feat: implement task 03 solution

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,20 @@
+const fsPromises = require('fs/promises');
+const path = require('path');
+
+const folderPath = path.join(__dirname, 'secret-folder');
+
+fsPromises.readdir(folderPath, { withFileTypes: true }).then(array => {
+  array.forEach(item => {
+    if (item.isFile()) {
+      const fileExt = path.extname(item.name).slice(1);
+      const fileName = item.name.replace(`.${ fileExt }`, '');
+      
+      fsPromises.stat(path.join(folderPath, item.name)).then(
+        result => {
+          const fileSize = `${ result.size / 1000 }kb`;
+          console.log(`${ fileName } - ${ fileExt } - ${ fileSize }`);
+        }
+      );
+    }
+  });
+});
