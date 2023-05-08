0eca42669b5cc7cc56c8a57e5676748fccbd6f9d
Yevhenii Khovaiev <ekhovaev@gmail.com>
2023-05-08 03:10:01 +0300

    feat: add more function options, export function

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -3,22 +3,23 @@ const createReadStream = require('fs').createReadStream;
 const createWriteStream = require('fs').createWriteStream;
 const path = require('path');
 
-const folderPath = path.join(__dirname, 'styles');
+const srcPath = path.join(__dirname, 'styles');
+const distPath = path.join(__dirname, 'project-dist', 'bundle.css');
 
-async function createBundle(from) {
-  const innerArray = await fsPromises.readdir(from, { withFileTypes: true });
-
+async function createBundle(srcPath, distPath) {
+  const innerArray = await fsPromises.readdir(srcPath, { withFileTypes: true });
+  
   const cssFilesArray = innerArray.filter(item => {
     return item.isFile() && path.extname(item.name) === '.css';
   });
 
   let bundle = [];
   for (const file of cssFilesArray) {
-    const readStream = createReadStream(path.join(from, file.name), 'utf-8');
-    const writeStream = createWriteStream(path.join(from, '..', 'project-dist', 'bundle.css'));
+    const readStream = createReadStream(path.join(srcPath, file.name), 'utf-8');
+    const writeStream = createWriteStream(distPath);
 
     readStream.on('data', chunk => {
-      bundle.push(chunk);
+      bundle.push(chunk + '\n');
     });
     readStream.on('end', () => {
       bundle.forEach(item => writeStream.write(item));
@@ -26,4 +27,8 @@ async function createBundle(from) {
   }
 }
 
-createBundle(folderPath);
+if (require.main === module) {
+  createBundle(srcPath, distPath);
+}
+
+module.exports = createBundle;
