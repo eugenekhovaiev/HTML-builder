1888f44ddcbfb2656ec318b84c832f9971f249ff
Yevhenii Khovaiev <ekhovaev@gmail.com>
2023-05-08 03:01:58 +0300

    refactor: remove unnecessary newline

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -9,12 +9,10 @@ fsPromises.readdir(folderPath, { withFileTypes: true }).then(array => {
       const fileExt = path.extname(item.name).slice(1);
       const fileName = item.name.replace(`.${ fileExt }`, '');
       
-      fsPromises.stat(path.join(folderPath, item.name)).then(
-        result => {
-          const fileSize = `${ result.size / 1000 }kb`;
-          console.log(`${ fileName } - ${ fileExt } - ${ fileSize }`);
-        }
-      );
+      fsPromises.stat(path.join(folderPath, item.name)).then(result => {
+        const fileSize = `${ result.size / 1000 }kb`;
+        console.log(`${ fileName } - ${ fileExt } - ${ fileSize }`);
+      });
     }
   });
 });
